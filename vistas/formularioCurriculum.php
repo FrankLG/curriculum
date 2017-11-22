eb8f224f416dfa654cdd842b83d0fd4d7a339f69
miguelille <32550594+miguelille@users.noreply.github.com>
2017-11-22 18:07:59 +0100

    Corregidos name en formularioCurriculum

--- vistas/formularioCurriculum.php
+++ vistas/formularioCurriculum.php
@@ -14,7 +14,7 @@
 <form>
     <fieldset class="titulacion">
         <h2>Titulación oficial</h2>
-        Titulo: <input type="text" name="titulo"> Centro<input type="text" name="centro"> Fecha inicio:<input type="date" name="fInicio"> Fecha fin:<input type="date" name="fFin"> Tipo: <select name="tipo">
+        Titulo: <input type="text" name="nombreti"> Centro<input type="text" name="centro"> Fecha fin:<input type="date" name="fechafin"> Tipo: <select name="tipo">
         <option>Grado universitario</option>
         <option>C.F.G.S.</option>
         <option>C.F.G.M.</option>
@@ -24,20 +24,37 @@
     </fieldset>
     <fieldset>
         <h2>Habilidades</h2>
-        <input type="text" name="habilidad"><br>
+        Habilidad: <input type="text" name="nombreha"><br>
+        Descripción: <textarea name="descripcion" rows="5" cols="50">Describe tu habilidad/conocimiento (como la conseguiste, como de bueno eres en ello...)</textarea><br>
         <input type="button" id="nuevaHab" value="Añadir habilidad">
     </fieldset>
     <fieldset>
         <h2>Idiomas</h2>
-        Nombre:<input type="text" name="idioma"> nivel:<input type="text" name="nivel"><br>
+        <select name="nombreid">
+            <option value="ingles">Inglés</option> 
+            <option value="frances">Francés</option> 
+            <option value="italiano">Italiano</option>
+            <option value="aleman">Alemán</option> 
+            <option value="arabe">Arabe</option> 
+            <option value="ruso">Ruso</option> 
+        </select>
+        <select name="nombreni">
+           <option value="a1">A1</option> 
+           <option value="a2">A2</option> 
+           <option value="b1">B1</option> 
+           <option value="b2">B2</option>
+           <option value="c1">C1</option> 
+           <option value="c2">C2</option> 
+        </select>
         <input type="button" id="nuevoIdi" value="Añadir idioma">
     </fieldset>
+    <fieldset>
     <h2>Otros</h2>
-    Carné de conducir <input type="checkbox" name="conducir">
-    Incorporación inmediata <input type="checkbox" name="incorporacion">
-    vehiculo propio<input type="checkbox" name="vehiculo">
-    Flexibilidad horaria<input type="checkbox" name="flexibilidad">
-    Disponibilidad geografica <input type="checkbox" name="geografica">
-    
+    Carné de conducir <input type="checkbox" name="conducir">  <br>
+    Incorporación inmediata <input type="checkbox" name="incorporacion"><br>
+    vehiculo propio<input type="checkbox" name="vehiculo"><br>
+    Flexibilidad horaria<input type="checkbox" name="flexibilidad"><br>
+    Disponibilidad geografica <input type="checkbox" name="geografica"><br>
+    </fieldset>
 </form>
 
