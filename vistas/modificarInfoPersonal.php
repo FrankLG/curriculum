f9ae960201f4bac5335fa02ff5385139c74b263a
miguelille <32550594+miguelille@users.noreply.github.com>
2017-11-29 13:18:29 +0100

    Por si acaso

--- vistas/modificarInfoPersonal.php
+++ vistas/modificarInfoPersonal.php
@@ -1,7 +1,7 @@
 <?php
 
     $info = $datos[0];
-   
+
     echo "<form action='index.php'>"
     . "Nombre: <input type='text' name='nombreal' value='".$info['nombreal']."'><br>"
             . "Apellidos: <input type='text' name='apellido' value='".$info['apellido']."'><br>"
@@ -11,7 +11,7 @@
             . "Localidad: <input type='text' name='localidad' value='".$info['localidad']."'><br>"
             . "Provincia: <input type='text' name='provincia' value='".$info['provincia']."'><br>"
             . "DNI: <input type='text' name='dni' value='".$info['dni']."'><br>";
-    
+
     if($info["activo"]==0){
         echo "Si <input type='radio' name='activo' value='1'>"
         . "No <input type='radio' name='activo' value='0' checked><br>";
